8bc66eb1d20593471387d2a3e596cfaadf0e2d7d
Good <85409973+Goodbs@users.noreply.github.com>
2022-10-03 22:33:27 +0300

    Update questsHscript.hx

--- questsHscript.hx
+++ questsHscript.hx
@@ -2,6 +2,18 @@ function endsong()
 {
 if(SONG.songName == "bopeebo")
 {
-File.saveContent("assets/data/questsData/Play Bopeebo.txt", "true");
+File.saveContent("assets/data/questsData/Play Bopeebo.txt", "Completed");
+}
+if(SONG.songName == "monster")
+{
+File.saveContent("assets/data/questsData/Monster.txt", "Completed");
+}
+if(SONG.songName == "blammed")
+{
+File.saveContent("assets/data/questsData/Blammed's Lights.txt", "Completed");
+}
+if(SONG.songName == "stress")
+{
+File.saveContent("assets/data/questsData/You Are NOT Tankman.txt", "Completed");
 }
 }
